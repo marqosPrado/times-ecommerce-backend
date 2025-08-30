df11a04d4b69426aeef913333d9878215b804ec3
Marcos Prado <marcospradofelix@gmail.com>
2025-08-29 21:18:13 -0300

    chore: initial commit

---
+++ src/main/java/br/com/marcosprado/timesbackend/TimesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.timesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcosprado/timesbackend/TimesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.timesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
