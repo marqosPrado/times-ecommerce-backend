b272fc5f9975c10b6f3f5692970eff74203287dc
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 17:48:35 -0300

    feat: enable CORS in ClientController to allow cross-origin requests

--- src/main/java/br/com/marcosprado/timesbackend/controller/ClientController.java
+++ src/main/java/br/com/marcosprado/timesbackend/controller/ClientController.java
@@ -5,11 +5,13 @@ import br.com.marcosprado.timesbackend.dto.ClientDto;
 import br.com.marcosprado.timesbackend.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@CrossOrigin(origins = "*")
 public class ClientController {
 
     private final ClientService clientService;
