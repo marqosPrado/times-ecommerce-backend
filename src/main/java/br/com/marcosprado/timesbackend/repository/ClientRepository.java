b13489b9efa29b9cec4090d724627ebace42e428
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 18:54:41 -0300

    feat: extend ClientRepository to support JPA specifications for enhanced query capabilities

--- src/main/java/br/com/marcosprado/timesbackend/repository/ClientRepository.java
+++ src/main/java/br/com/marcosprado/timesbackend/repository/ClientRepository.java
@@ -2,12 +2,13 @@ package br.com.marcosprado.timesbackend.repository;
 
 import br.com.marcosprado.timesbackend.aggregate.ClientAggregate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<ClientAggregate, Integer> {
+public interface ClientRepository extends JpaRepository<ClientAggregate, Integer>, JpaSpecificationExecutor<ClientAggregate> {
     Optional<ClientAggregate> findByCpf(String cpf);
     Boolean existsByCpf(String cpf);
     Boolean existsByEmail(String email);
