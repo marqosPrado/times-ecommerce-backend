44ba92a849b150811ae2b5ff42d35350e51a0854
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 00:11:37 -0300

    feat: add AddressDto record for address data transfer

---
+++ src/main/java/br/com/marcosprado/timesbackend/dto/AddressDto.java
@@ -0,0 +1,17 @@
+package br.com.marcosprado.timesbackend.dto;
+
+import br.com.marcosprado.timesbackend.enums.TypePlace;
+import br.com.marcosprado.timesbackend.enums.TypeResidence;
+
+public record AddressDto(
+        TypeResidence typeResidence,
+        TypePlace typePlace,
+        String street,
+        Integer number,
+        String neighborhood,
+        String cep,
+        String country,
+        String observations,
+        Integer stateId
+) {
+}
