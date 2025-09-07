65c420216a60ed9b211d894cf9bee1741f0c8208
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 23:12:51 -0300

    feat: implement get client by id

---
+++ src/main/java/br/com/marcosprado/timesbackend/dto/client/ClientResponseCompleteDto.java
@@ -0,0 +1,46 @@
+package br.com.marcosprado.timesbackend.dto.client;
+
+import br.com.marcosprado.timesbackend.aggregate.ClientAggregate;
+import br.com.marcosprado.timesbackend.dto.AddressDto;
+import br.com.marcosprado.timesbackend.dto.CreditCardDto;
+import br.com.marcosprado.timesbackend.enums.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record ClientResponseCompleteDto(
+        String fullName,
+        String phoneNumber,
+        Gender gender,
+        String email,
+        String cpf,
+        LocalDate birthDate,
+        List<AddressDto> addresses,
+        List<CreditCardDto> creditCards
+) {
+    public static ClientResponseCompleteDto fromEntity(ClientAggregate client) {
+        List<AddressDto> addresses = client.getAddresses() != null
+                ? client.getAddresses().stream()
+                .map(AddressDto::fromEntity)
+                .collect(Collectors.toList())
+                : List.of();
+
+        List<CreditCardDto> cards = client.getCreditCards() != null
+                ? client.getCreditCards().stream()
+                .map(CreditCardDto::fromEntity)
+                .collect(Collectors.toList())
+                : List.of();
+
+        return new ClientResponseCompleteDto(
+                client.getFullName(),
+                client.getPhoneNumber(),
+                client.getGender(),
+                client.getEmail(),
+                client.getCpf(),
+                client.getBirthDate(),
+                addresses,
+                cards
+        );
+    }
+}
