d9b98ead53eb2bf681ab0b22d60aab2a3efd551d
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 23:15:20 -0300

    feat: implement method to get client by id on controller

--- src/main/java/br/com/marcosprado/timesbackend/controller/ClientController.java
+++ src/main/java/br/com/marcosprado/timesbackend/controller/ClientController.java
@@ -2,13 +2,11 @@ package br.com.marcosprado.timesbackend.controller;
 
 import br.com.marcosprado.timesbackend.aggregate.ClientAggregate;
 import br.com.marcosprado.timesbackend.dto.client.ClientDto;
+import br.com.marcosprado.timesbackend.dto.client.ClientResponseCompleteDto;
 import br.com.marcosprado.timesbackend.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @CrossOrigin(origins = "*")
@@ -29,5 +27,10 @@ public class ClientController {
             throw new RuntimeException("Erro ao registrar cliente: " + e.getMessage());
 
         }
+    }
+
+    @GetMapping("/client/{id}/all")
+    public ResponseEntity<ClientResponseCompleteDto> getClient(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getClient(id));
     }
 }
