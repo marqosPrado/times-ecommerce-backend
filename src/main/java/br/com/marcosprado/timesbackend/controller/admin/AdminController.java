358c70a8e6e3aab2a21ca5c930dc083c0f8dc08c
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 22:27:21 -0300

    feat: implement method to handle client status on controller

--- src/main/java/br/com/marcosprado/timesbackend/controller/admin/AdminController.java
+++ src/main/java/br/com/marcosprado/timesbackend/controller/admin/AdminController.java
@@ -1,17 +1,19 @@
 package br.com.marcosprado.timesbackend.controller.admin;
 
 import br.com.marcosprado.timesbackend.dto.admin.ClientFilterDto;
+import br.com.marcosprado.timesbackend.dto.admin.UpdateClientStatusDto;
 import br.com.marcosprado.timesbackend.dto.client.ClientResponseDto;
 import br.com.marcosprado.timesbackend.service.AdminService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController()
+@CrossOrigin(origins = "*")
 public class AdminController {
 
     private final AdminService adminService;
@@ -21,11 +23,28 @@ public class AdminController {
     }
 
     @GetMapping("admin/client/search")
-    public ResponseEntity<List<ClientResponseDto>> filterClient(@ModelAttribute ClientFilterDto filter) throws Exception {
+    public ResponseEntity<Page<ClientResponseDto>> filterClient(
+            @ModelAttribute ClientFilterDto filter,
+            Pageable pageable
+    ) throws Exception {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(adminService.search(filter));
+            return ResponseEntity.status(HttpStatus.OK).body(adminService.search(filter, pageable));
         } catch (Exception e) {
             return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PatchMapping("admin/client/{id}/status")
+    public ResponseEntity<?> updateClientStatus(
+            @PathVariable("id") Integer id,
+            @RequestBody UpdateClientStatusDto dto) {
+        try {
+            ClientResponseDto updatedClient = adminService.updateStatus(id, dto);
+            return ResponseEntity.ok(updatedClient);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o status do cliente.");
         }
     }
 }
